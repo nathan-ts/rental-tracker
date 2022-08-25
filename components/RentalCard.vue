29ea78335e6809dbfde422716800dbf65f01c13d
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2022-08-25 13:03:42 -0400

    adjust styling on card

--- components/RentalCard.vue
+++ components/RentalCard.vue
@@ -1,8 +1,16 @@
 <template>
-	<div>
-		<p>Rental Card</p>
-		<p>This is a rental card.</p>
-		<font-awesome-icon :icon="['fas', 'angle-down']"/>
+	<div class="office-card
+		bg-white
+		m-4 p-6
+		rounded-lg
+		shadow-xl
+		flex flex-row justify-between items-center
+	">
+		<div>
+			<div class="h3 text-2xl font-semibold">Rental</div>
+			<div class="h4 text-base text-gray-500">24 Sussex Dr.</div>
+		</div>
+		<font-awesome-icon :icon="['fas', 'angle-down']" class="text-cyan-600 text-2xl"/>
 	</div>
 </template>
 
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,13 @@
 <template>
 	<div class="h-screen bg-gray-100">
-		<div class="h-screen flex flex-col border-2 border-black mx-auto sm:w-1/2 md:w-96 ">
+		<div class="
+			h-screen
+			flex flex-col
+			border-2 border-black
+			mx-auto
+			min-w-min
+			max-w-lg
+		">
 			<Header />
 			<nuxt />
 			<Footer />

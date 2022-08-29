b6087819b323956c7416f3eabf301b48932f9274
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2022-08-29 15:39:50 -0400

    add css for error !

--- components/InputField.vue
+++ components/InputField.vue
@@ -5,9 +5,14 @@
 		<div class="pb-1">{{ label }}</div>
 		<input
 			type="text" :name="name" :value="value"
-			@input="$emit('input', $event.target.value)"
+			@input="onInput"
 			required="required"
-			class="w-full border border-gray-400 rounded-md p-2"
+			class="w-full border border-gray-400 bg-transparent rounded-md p-2"
+		/>
+		<font-awesome-icon
+			:icon="['fas', 'circle-exclamation']"
+			class="text-xl text-red-300 float-right mr-3 -mt-8"
+			visibility=hidden
 		/>
 	</label>
 </div>
@@ -25,5 +30,16 @@ export default {
       type: String,
     }
   },
+	methods: {
+    onInput(event) {
+      this.$emit('input', event.target.value);
+    }
+  },
 }
 </script>
+
+<style type="text/css">
+	.errspan {
+		position: relative;
+	}
+</style>

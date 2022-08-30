fd91973098b3b17b1d1dd4446d9b0c9c2ad95765
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2022-08-30 11:40:26 -0400

    add mock data

---
+++ data/mock.js
@@ -0,0 +1,43 @@
+export default mockData = [{
+  "title": "Melby",
+  "address": "579 Melby Court",
+  "name": "August Staite",
+  "position": "Analog Circuit Design manager",
+  "email": "astaite0@gravatar.com",
+  "phone": "(291) 8402192"
+}, {
+  "title": "Stoughton",
+  "address": "016 Stoughton Road",
+  "name": "Rosamund Gethin",
+  "position": "Registered Nurse",
+  "email": "rgethin1@springer.com",
+  "phone": "(500) 4100426"
+}, {
+  "title": "Eagan",
+  "address": "188 Eagan Crossing",
+  "name": "Christy Depport",
+  "position": "VP Quality Control",
+  "email": "cdepport2@yellowbook.com",
+  "phone": "(372) 9134705"
+}, {
+  "title": "Springs",
+  "address": "3338 Springs Circle",
+  "name": "Addie Fishbourne",
+  "position": "Junior Executive",
+  "email": "afishbourne3@paypal.com",
+  "phone": "(824) 9482124"
+}, {
+  "title": "Roxbury",
+  "address": "98887 Roxbury Way",
+  "name": "Avictor Knatt",
+  "position": "Financial Analyst",
+  "email": "aknatt4@statcounter.com",
+  "phone": "(942) 6746786"
+}, {
+  "title": "Crest Line",
+  "address": "3 Crest Line Place",
+  "name": "Barbabra Emberson",
+  "position": "VP Accounting",
+  "email": "bemberson5@friendfeed.com",
+  "phone": "(457) 7380738"
+}];

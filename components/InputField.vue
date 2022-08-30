b4a3af785b43a7a2f20c45e94bfdf0d4501fb5d2
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2022-08-30 11:35:39 -0400

    remove emit and use v-model instead

--- components/InputField.vue
+++ components/InputField.vue
@@ -1,75 +1,84 @@
 <template>
 
-<div class="text-base">
-	<label>
-		<div class="pb-1">{{ label }}</div>
-		<input
-			type="text" :name="name" :value="value"
-			@input="onInput"
-			required="required"
-			:class="{
-				'w-full': true,
-				border: true,
-				'border-gray-400': !this.error,
-				'border-red-300': this.error,
-				'bg-transparent': true,
-				'rounded-md': true,
-				'p-2': true,
-			}"
-		/>
-		<font-awesome-icon
-			:icon="['fas', 'circle-exclamation']"
-			class="text-xl text-red-300 float-right mr-3 -mt-8"
-			v-show="error"
-		/>
-	</label>
-	<div v-if="error" class="text-sm text-red-300 pt-1">{{ error }}</div>
-</div>
-
-</template>
-
-<script>
-export default {
-  props: {
-    label: {
-			required: true,
-      type: String,
-    },
-		value: {
-      type: String,
-    }
-  },
-	data() {
-    return {
-      error: '',
-			exclaimVisible: this.error ? "visible" : "hidden",
-    };
-  },
-	methods: {
-		// onInput sets an error if the field is changed and empty
-    onInput(event) {
-			const value = event.target.value;
-      if (!value) {
-        this.error = 'This field cannot be empty';
+  <div class="text-base">
+    <label>
+      <div class="pb-1">{{ label }}</div>
+      <input
+        type="text"
+        v-model="model"
+        required="required"
+        :class="{
+          'border-gray-400': !this.error,
+          'border-red-300': this.error,
+          'border': true,
+          'w-full': true,
+          'bg-transparent': true,
+          'rounded-md': true,
+          'p-2': true,
+        }"
+      />
+      <font-awesome-icon
+        :icon="['fas', 'circle-exclamation']"
+        class="text-xl text-red-300 float-right mr-3 -mt-8"
+        v-show="error"
+      />
+    </label>
+    <div v-if="error" class="text-sm text-red-300 pt-1">{{ error }}</div>
+  </div>
+  
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      label: {
+        required: true,
+        type: String,
+      },
+      value: {
+        type: String,
       }
-      this.$emit('input', value);
-    }
-  },
-	// watcher removes error whenever value is not empty
-	watch: {
-    value: {
-      handler(value) {
-        if (value) {
-          this.error = '';
-        }
+    },
+    computed: {
+      model: {
+        get() {
+          return this.value;
+        },
+        set(value) {
+          if (!value) {
+            this.error = 'This field cannot be empty';
+          }
+          this.$emit('input', value);
+        },
       },
     },
-  },
-}
-</script>
-
-<style type="text/css">
-	.errspan {
-		position: relative;
-	}
-</style>
+    data() {
+      return {
+        error: '',
+      };
+    },
+    // methods: {
+    // 	// onInput sets an error if the field is changed and empty
+    //   onInput(event) {
+    // 		const value = event.target.value;
+    //     if (!value) {
+    //       this.error = 'This field cannot be empty';
+    //     }
+    //     this.$emit('input', value);
+    //   }
+    // },
+    // watcher removes error whenever value is not empty
+    // watch: {
+    //   value: {
+    //     handler(value) {
+    //       if (value) {
+    //         this.error = '';
+    //       }
+    //     },
+    //   },
+    // },
+  }
+  </script>
+  
+  <style>
+  </style>
